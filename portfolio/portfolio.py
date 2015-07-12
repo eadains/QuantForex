133c5a15fac87d6bed40c8ce1d2f4365f7572902
Erik Dains <eadains@gmail.com>
2015-07-12 17:10:39 -0400

    Working on Portfolio class.

--- portfolio/portfolio.py
+++ portfolio/portfolio.py
@@ -1,4 +1,5 @@
-
+import Decimal
+from portfolio.position import Position
 
 
 class Portfolio(object):
@@ -10,9 +11,9 @@ class Portfolio(object):
     """
 
     def __init__(
-        self, data_stream, events_queue, home_currency="USD", 
-        leverage=20, equity=Decimal("100000.0"), bactest=None
-        ):
+        self, data_stream, events_queue, home_currency="USD",
+        leverage=20, equity=Decimal("100000.0"), backtest=None
+            ):
 
         """
         data_stream = Streaming object. Object must be subclassed from PriceHandler
@@ -34,3 +35,63 @@ class Portfolio(object):
         self.positions = {}
         if self.backtest:
             self.backtest_file = self.backtest.backtest_file_dir
+
+    def add_new_position(self, side, currency_pair, units):
+
+        """
+        Adds new position into positions dictionary.
+        side = "long" or "short"
+        currency_pair = currency pair being traded
+        units = number of currency units long/short
+        """
+
+        position = Position(
+            self.home_currency, side, currency_pair,
+            units, self.data_stream
+        )
+        self.positions[currency_pair] = position
+
+    def add_position_units(self, currency_pair, units):
+
+        """
+        Adds units to an exsisting Position,
+        denoted by the currency_pair
+        """
+
+        if currency_pair not in self.positions:
+            return False
+        else:
+            position = self.positions[currency_pair]
+            position.add_units(units)
+            return True
+
+    def remove_position_units(self, currency_pair, units):
+
+        """
+        Removes units from an existing Position,
+        denoted by currency_pair
+        """
+
+        if currency_pair not in self.positions:
+            return False
+        else:
+            position = self.positions[currency_pair]
+            pnl = position.remove_units(units)
+            self.balance += pnl
+            return True
+
+    def close_position(self, currency_pair):
+
+        """
+        Closes exsiting position,
+        as denoted by currency_pair
+        """
+
+        if currency_pair not in self.positions:
+            return False
+        else:
+            position = self.positions[currency_pair]
+            pnl = position.close_position()
+            self.balance += pnl
+            del[self.positions[currency_pair]]
+            return True
